c830d4858e1c57379187e980d16ea124c9abb955
Juhodev <jooo.dev@gmail.com>
2020-09-30 13:57:25 +0300

    Add a route for getting and creating tags

--- backend/src/routes/tagsRoute.ts
+++ backend/src/routes/tagsRoute.ts
@@ -1,4 +1,17 @@
 import expressPromiseRouter from 'express-promise-router';
+import { itemManager } from '..';
+import { TagResponse } from '../inventory/types';
 const router = expressPromiseRouter();
 
-router.get('/', (req, res) => {});
+router.get('/', (req, res) => {
+	res.json({ error: false, tags: itemManager.getTags().getAll() });
+});
+
+router.post('/add', (req, res) => {
+	const { body } = req;
+	const { name } = body;
+
+	const response: TagResponse = itemManager.getTags().add(name);
+
+	res.json(response);
+});

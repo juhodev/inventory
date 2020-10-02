e149d923dcb5fad65792f89a67728010fcd58274
Juhodev <jooo.dev@gmail.com>
2020-10-03 01:32:40 +0300

    Change the pointer when hovering an item

--- frontend/src/components/inventory/item.tsx
+++ frontend/src/components/inventory/item.tsx
@@ -14,15 +14,15 @@ const ItemComponent = (props: ItemComponentProps) => {
 					<span
 						onClick={() => onTagClick(tag)}
 						key={tag}
-						className="text-lg py-2 px-5 m-2 h-10 rounded-full font-bold text-gray-300 bg-blue-500"
+						className="text-lg py-2 px-5 m-2 h-10 rounded-full font-bold text-gray-300 bg-blue-500 cursor-pointer"
 					>
 						{tag}
 					</span>
 			  ));
 
 	return (
-		<div className="container flex flex-row h-42 mx-auto px-4 p-6 border-b-2 border-indigo-600 font-body cursor-pointer">
-			<div className="flex justify-center items-center w-32 h-32">
+		<div className="container flex flex-row h-42 mx-auto px-4 p-6 border-b-2 border-indigo-600 font-body">
+			<div className="flex justify-center items-center w-32 h-32 cursor-pointer">
 				<img
 					className="max-w-20 rounded-lg"
 					onClick={onClick}
@@ -33,7 +33,7 @@ const ItemComponent = (props: ItemComponentProps) => {
 					}
 				/>
 			</div>
-			<div className="pl-6 flex flex-col h-full" onClick={onClick}>
+			<div className="pl-6 flex flex-col h-full cursor-pointer" onClick={onClick}>
 				<div className="flex flex-col w-full">
 					<span className="font-bold text-3xl leading-none text-blue-500">
 						{name}

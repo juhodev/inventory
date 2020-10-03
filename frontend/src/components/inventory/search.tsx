9147e6ae778f5eb8be622c1aa04e4bd2d555937c
Juhodev <jooo.dev@gmail.com>
2020-10-03 11:04:53 +0300

    Change the search box style

--- frontend/src/components/inventory/search.tsx
+++ frontend/src/components/inventory/search.tsx
@@ -7,7 +7,7 @@ const SearchComponent = (props: SearchComponentProps) => {
 	return (
 		<div className="flex flex-grow justify-center">
 			<input
-				className="border border-blue-800 h-10 p-2 font-body rounded w-80 bg-blue-500 text-gray-200"
+				className="border-b-2 border-indigo-600 bg-transparent h-10 p-2 font-body w-80 text-gray-200"
 				placeholder="Search..."
 				value={value}
 				onChange={(e) => {
